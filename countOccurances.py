06b805e14c23822108b167c7252c3bc7255b26fe
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-07-12 22:51:56 +0530

    Create countOccurances.py

---
+++ countOccurances.py
@@ -0,0 +1,14 @@
+# Counting the Number of Occurances of a Character in a String
+
+
+word = "programming"
+letter = 'm'
+
+count = 0
+
+
+for character in word:
+	if character == letter:
+    	 count += 1
+        
+print(count) #--> 2
