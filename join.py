e5e91edb99a1d70b952ebade7a84c2391a387d88
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-07-12 23:22:17 +0530

    Create join.py

---
+++ join.py
@@ -0,0 +1,17 @@
+#.join() keyword uses in case of tuple, list and dict
+
+myTuple = ("John", "Peter", "Vicky")
+myList = ["John", "Peter", "Vicky"]
+myDict = {"user1":"John", "user2":"Peter", "user3":"Vicky"}
+
+seperator = "#"
+
+x = seperator.join(myTuple)
+y = seperator.join(myList)
+zKey = seperator.join(myDict)
+zValue = seperator.join(myDict.values())
+
+print(x) #--> John#Peter#Vicky
+print(y) #--> John#Peter#Vicky
+print(zKey) #--> user1#user2#user3
+print(zValue) #--> John#Peter#Vicky
