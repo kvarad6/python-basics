02226e39f1ddf55c7051051550f2c7717ae5867d
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-07-28 13:17:57 +0530

    Create ascendingSort.py | Without .sort()

---
+++ ascendingSort.py
@@ -0,0 +1,16 @@
+# Sort a list in ascending without using list.sort
+
+# approach 1 | using new list | extra space
+originalList = [2,1,5,4,3]
+newList = []
+
+while originalList:
+    minimum = originalList[0]
+    for num in originalList:
+        if minimum>num:
+            minimum = num
+    newList.append(minimum)
+    originalList.remove(minimum)
+        
+print(newList)
+    
