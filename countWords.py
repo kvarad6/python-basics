1336cfbf7da4736e141d11a75a462eec27f97d51
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-07-28 23:28:59 +0530

    Create countWords.py

---
+++ countWords.py
@@ -0,0 +1,8 @@
+# Program to print number of words in a sentence
+
+sentence = "I will achieve it one day"
+
+#.split() -> splits the sentence into list of words/ strings
+print(sentence.split())
+
+print(len(sentence.split()))
