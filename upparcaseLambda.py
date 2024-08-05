348c50e8dc093bc7a899d8d0c4314cf6cfdbbf52
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-05 08:29:12 +0530

    Create upparcaseLambda.py | String | Uppercase

---
+++ upparcaseLambda.py
@@ -0,0 +1,7 @@
+#String to upparcase using lambda function
+
+to_uppercase = lambda text: text.upper()
+
+message = "Hello, world!"
+uppercase_message = to_uppercase(message)
+print(uppercase_message)  # Output: HELLO, WORLD!
