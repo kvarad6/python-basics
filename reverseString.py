04e3ff48238910402deb1b41c376653201631636
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-15 17:53:06 +0530

    Update reverseString.py | slice | join + sorted | difference

--- reverseString.py
+++ reverseString.py
@@ -1,3 +1,15 @@
-string = "sample"
+#reversing string using join and sorted vs using slice
+# It first alphabetically sorts the string
+# Then reverse the alphabetically sorted string
+# So the result is not similar to one getting using slice (string[::-1])
 
-print(string[::-1]) #--> elpmas
+
+string = "varad"
+sortedString = ''.join(sorted(string)) 
+reverseSortedString = ''.join(sorted(string, reverse=True)) 
+print(sortedString)  # #hiimrs
+print(reverseSortedString)
+
+#reversing using slice
+string2 = "varad"
+print(string2[::-1]) #darav
