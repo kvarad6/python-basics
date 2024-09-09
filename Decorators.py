ab0db5719544f02d54b393c18da8343ec4e93ce0
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-09 16:44:09 +0530

    Create Decorators.py

---
+++ Decorators.py
@@ -0,0 +1,26 @@
+class DecoratorClass:
+    def __init__(self, originalFunction):
+        self.originalFunction = originalFunction
+    
+    def __call__(self, *args, **kwargs):
+        print("__call__ method called this before {}".format(self.originalFunction.__name__))
+        return self.originalFunction(*args, **kwargs)
+
+@DecoratorClass
+def display():
+    print("This is a display function")
+    
+@DecoratorClass
+def displayInfo(name, age):
+    print("displayInfo ran with the arguments {} and {}".format(name, age))
+
+display()
+#prints: 
+# __call__ method called this before display
+# This is a display function
+
+
+displayInfo("bob", 15)
+#prints:
+# __call__ method called this before displayInfo
+# displayInfo ran with the arguments bob and 15
