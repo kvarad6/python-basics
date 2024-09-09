a632e2115c07a8cccd6bedabcc2d2b5fa5ac583d
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-09 17:31:36 +0530

    Update Decorators.py

--- Decorators.py
+++ Decorators.py
@@ -53,4 +53,58 @@ def displayInfo(name, age):
     print("I am {} of {} age".format(name, age)) 
     
 displayInfo("Bob", 15)
+
+
+#-------------------------------------------------------------#
+#Using multiple decorators
+
+#timer decorator function
+import time
+def timer(originalFunction):
+    def wrapper(*args, **kwargs):
+        #tracking the start time
+        t1 = time.time()
+        
+        #running the function
+        result = originalFunction(*args, **kwargs)
+        
+        #tracking the end time
+        t2 = time.time()
+        print("The function {} run for {}".format(originalFunction.__name__, t2-t1)) #The function displayInfo run for 3.8623809814453125e-05
+        return result
+        
+    return wrapper
+
+#logger decorator function
+def logger(originalFunction):
+    def wrapper(*args, **kwargs):
+        print("{} function is running with the arguments: ({}, {})".format(originalFunction.__name__, *args))
+        return originalFunction(*args, **kwargs)
+    return wrapper
+
+
+#calling multiple decorators
+@logger
+@timer
+def displayInfo(name, age):
+    print("I am {} of {} age".format(name, age)) 
+    
+displayInfo("Bob", 15)
+
+"""
+Output:
+wrapper function is running with the arguments: (Bob, 15)
+I am Bob of 15 age
+The function displayInfo run for 6.198883056640625e-06
+"""
+
+"""
+@logger
+@timer
+def displayInfo(name, age)
+
+can also be written as:
+displayInfo = logger(timer(displayInfo("bob", 15)))
+"""
+        
         
