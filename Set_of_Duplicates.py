e56d15931c9753fbfdd68a2ded3098082d75b4e6
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-15 02:18:15 +0530

    Update Set_of_Duplicates.py

--- Set_of_Duplicates.py
+++ Set_of_Duplicates.py
@@ -16,3 +16,21 @@ print(count)  #-> Counter({3: 2, 6: 2, 1: 1, 2: 1, 4: 1, 5: 1})
 print(count.items()) #-> dict_items([(1, 1), (2, 1), (3, 2), (4, 1), (5, 1), (6, 2)])
 
 print(set(num for num, count in count.items() if count>1)) #-> {3, 6}
+
+
+#-------- Approach 3 ----------#
+#using dict
+
+freqDict = {}
+
+for item in lst:
+    if item in freqDict:
+        freqDict[item] += 1
+    else:
+        freqDict[item] = 1
+
+duplicatesCount = 0
+
+for item, count in freqDict.items():
+    if count>1:
+        print(item)
