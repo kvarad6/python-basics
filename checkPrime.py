20d501107ffd07a59c730a38e06b49bda2ccd56c
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-14 21:57:36 +0530

    Update checkPrime.py

--- checkPrime.py
+++ checkPrime.py
@@ -6,3 +6,38 @@ for i in range(2,num):
         break;
 else:
     print("prime")
+
+
+#to check if number is prime or not
+
+#---------- Approach 1 ---------#
+#prime no-> divible by only 1 and itself and has only 2 divisors
+#TC: O(n)
+#SC: O(1)
+n = 5
+
+count = 0
+
+for i in range(1, n+1):
+    if n%i == 0:
+        count += 1
+
+if count == 2:
+    print("the number is prime")
+else:
+    print("the number is not a prime")
+
+#-------- Approach 2 ---------#
+
+#no need to calculate till n, just calculate till n//2
+#or we can also achieve the same by calculating till squareroot of n
+n = 10
+count = 0
+
+for i in range(2, (n//2)+1):
+    if n%i == 0:
+        print("the number is not a prime")
+        break
+else:
+    print("the number is a prime number")
+    
