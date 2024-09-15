c53d19ae82c016199405de3cb5a9acc6da6b0015
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-16 01:09:02 +0530

    Update sortDict_lambda.py

--- sortDict_lambda.py
+++ sortDict_lambda.py
@@ -1,4 +1,4 @@
-#Sort a list of dictionaries by the last name key
+#sorting a list of dicts by "last" key
 
 people = [
     {"first": "Alice", "last": "Smith"},
@@ -6,9 +6,21 @@ people = [
     {"first": "Charlie", "last": "Brown"},
 ]
 
+#-------- Appraoch 1 ---------#
 
 sortByLastName = lambda person: person["last"]
 
 people.sort(key=sortByLastName)
 
+#can also be written as:
+people.sort(key = lambda person: person["last"])
+
 print(people) #-> [{'first': 'Charlie', 'last': 'Brown'}, {'first': 'Bob', 'last': 'Johnson'}, {'first': 'Alice', 'last': 'Smith'}]
+
+
+#------ Approach 2 ------#
+sortedList = sorted(people, key= lambda person: person["last"])
+
+print(sortedList) #-> [{'first': 'Charlie', 'last': 'Brown'}, {'first': 'Bob', 'last': 'Johnson'}, {'first': 'Alice', 'last': 'Smith'}]
+
+
